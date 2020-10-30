3eefff8400d16dc35dcc46fdc6ca693bbb57a681
lucatrazzi <luca.trazzi@garaio.com>
2020-10-30 14:46:07 +0100

    updated to 3.1

--- src/Kairos.Application/UserProfile/Commands/CreateOrUpdateUserProfile.cs
+++ src/Kairos.Application/UserProfile/Commands/CreateOrUpdateUserProfile.cs
@@ -3,7 +3,7 @@ using UserProfileModel = Kairos.Application.UserProfile.Dtos.UserProfileModel;
 
 namespace Kairos.Application.UserProfile.Commands
 {
-    public class CreateOrUpdateUserProfile : Command<string?>
+    public class CreateOrUpdateUserProfile : Command<string>
     {
         public CreateOrUpdateUserProfile(UserProfileModel userProfile)
         {
